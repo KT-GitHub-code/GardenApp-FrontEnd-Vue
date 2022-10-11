18cbf3432d80fd6296ca2f4f0bbb2b46f86e1314
KT <krakovszkyt@gmail.com>
2022-10-11 16:08:25 +0200

    Add NewPlant component

---
+++ src/components/NewPlant.vue
@@ -0,0 +1,22 @@
+<template>
+    <div className='container'>    
+      <div className='card-container'>
+        <div className='image-container'>
+          <img src="depositphotos_69303973-stock-photo-pharmacy-plus-with-ring-sign.jpg" width="200" height="200" alt=""/>
+        </div>
+        <div className='card-content'>
+          <div className='card-title'>
+            <p>Need a new plant? :)</p>
+          </div>
+
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'NewPlant',
+}
+</script>
+
--- src/components/PlantList.vue
+++ src/components/PlantList.vue
@@ -1,17 +1,22 @@
 <template>
-    <div className='container' :key="plant.id" v-for="plant in plants">
+    <div>
+        <div className='container' :key="plant.id" v-for="plant in plants">
         <Plant :plant="plant"/>
+        </div>
+        <NewPlant className='container'/>    
     </div>
+
 </template>
 
 <script>
 import Plant from './Plant.vue';
+import NewPlant from './NewPlant.vue';
 
 export default {
     name: "PlantList",
     props: {
         plants: Array
     },
-    components: { Plant }
+    components: { Plant, NewPlant }
 }
 </script>
